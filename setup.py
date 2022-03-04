67339ffb862baa19a0c57ae25184afe766cac224
Mikael <mikael@bitcanon.com>
2022-03-04 21:07:41 +0100

    Prepare for uploading to PyPI.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='synochat',
+    version='1.0.0',
+    author='Mikael Schultz',
+    author_email='mikael@bitcanon.com',
+    description='A library for the Synology Chat API written in Python 3.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bitcanon/synochat',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
